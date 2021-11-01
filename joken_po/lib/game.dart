dabe6bc3a361ad802f1404ae3ca4876ae1cfdb2c
Lays Hidani <lays.hidani@gmail.com>
2021-11-01 17:00:01 -0300

    feat: add winner logic

--- joken_po/lib/game.dart
+++ joken_po/lib/game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -8,11 +9,60 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  var _appChoiceImage = AssetImage('images/padrao.png');
+  var _message = 'Faça sua escolha :)';
+
+  void _onSelectChoice(String userChoice) {
+    var options = ['rock', 'paper', 'scissors'];
+    var randomNumber = Random().nextInt(3);
+    var appChoice = options[randomNumber];
+
+    switch(appChoice) {
+      case 'rock':
+        setState(() {
+          _appChoiceImage = AssetImage('images/pedra.png');
+        });
+        break;
+      case 'paper':
+        setState(() {
+          _appChoiceImage = AssetImage('images/papel.png');
+        });
+        break;
+      case 'scissors':
+        setState(() {
+          _appChoiceImage = AssetImage('images/tesoura.png');
+        });
+        break;
+    }
+
+    if (
+      (userChoice == 'rock' && appChoice == 'scissors')
+      || (userChoice == 'scissors' && appChoice == 'paper')
+      || (userChoice == 'paper' && appChoice == 'rock')
+    ) {
+      setState(() {
+        _message = 'Você ganhou! :D';
+      });
+    } else if (
+      (appChoice == 'rock' && userChoice == 'scissors')
+      || (appChoice == 'scissors' && userChoice == 'paper')
+      || (appChoice == 'paper' && userChoice == 'rock')
+    ) {
+      setState(() {
+        _message = 'Você perdeu! :(';
+      });
+    } else {
+      setState(() {
+        _message = 'EMPATE!';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('JokenPo'),
+        title: const Text('JokenPo'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -28,13 +78,13 @@ class _GameState extends State<Game> {
               ),
             ),
           ),
-          Image.asset('images/padrao.png'),
-          const Padding(
+          Image(image: _appChoiceImage,),
+          Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
-              'Escolha do adversário (APP)',
+              _message,
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
@@ -43,9 +93,18 @@ class _GameState extends State<Game> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Image.asset('images/pedra.png', height: 100),
-              Image.asset('images/papel.png', height: 100),
-              Image.asset('images/tesoura.png', height: 100),
+              GestureDetector(
+                onTap: () => _onSelectChoice('rock'),
+                child: Image.asset('images/pedra.png', height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _onSelectChoice('scissors'),
+                child: Image.asset('images/tesoura.png', height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _onSelectChoice('paper'),
+                child: Image.asset('images/papel.png', height: 100),
+              ),
             ],
           ),
         ],
