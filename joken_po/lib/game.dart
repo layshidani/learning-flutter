1f53155a8c5793574ccf95df006e8e4e267ebde2
Lays Hidani <lays.hidani@gmail.com>
2021-11-01 17:42:42 -0300

    refactor: set image name

--- joken_po/lib/game.dart
+++ joken_po/lib/game.dart
@@ -9,31 +9,17 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  var _appChoiceImage = AssetImage('images/padrao.png');
-  var _message = 'Faça sua escolha :)';
+  var _appChoiceImage = AssetImage('images/default.png');
+  var _message = '';
 
   void _onSelectChoice(String userChoice) {
-    var options = ['rock', 'paper', 'scissors'];
+    var optionsArr = ['rock', 'paper', 'scissors'];
     var randomNumber = Random().nextInt(3);
-    var appChoice = options[randomNumber];
+    var appChoice = optionsArr[randomNumber];
 
-    switch(appChoice) {
-      case 'rock':
-        setState(() {
-          _appChoiceImage = AssetImage('images/pedra.png');
-        });
-        break;
-      case 'paper':
-        setState(() {
-          _appChoiceImage = AssetImage('images/papel.png');
-        });
-        break;
-      case 'scissors':
-        setState(() {
-          _appChoiceImage = AssetImage('images/tesoura.png');
-        });
-        break;
-    }
+    setState(() {
+      _appChoiceImage = AssetImage('images/$appChoice.png');
+    });
 
     if (
       (userChoice == 'rock' && appChoice == 'scissors')
@@ -83,15 +69,15 @@ class _GameState extends State<Game> {
             children: <Widget>[
               GestureDetector(
                 onTap: () => _onSelectChoice('rock'),
-                child: Image.asset('images/pedra.png', height: 100),
+                child: Image.asset('images/rock.png', height: 100),
               ),
               GestureDetector(
                 onTap: () => _onSelectChoice('scissors'),
-                child: Image.asset('images/tesoura.png', height: 100),
+                child: Image.asset('images/scissors.png', height: 100),
               ),
               GestureDetector(
                 onTap: () => _onSelectChoice('paper'),
-                child: Image.asset('images/papel.png', height: 100),
+                child: Image.asset('images/paper.png', height: 100),
               ),
             ],
           ),
