85c9b045dd036a5f4801156aa2b5b26b224b08ac
Lays Hidani <lays.hidani@gmail.com>
2021-11-01 12:26:18 -0300

    feat: add logo and changes style

--- random-phrases/lib/main.dart
+++ random-phrases/lib/main.dart
@@ -15,9 +15,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.cyan,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Gerador de frases'),
+      home: const MyHomePage(title: 'Frase do dia'),
     );
   }
 }
@@ -48,46 +48,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Pressione o botão para gerar uma frase',
+            const Center(
+              child: Image(
+                image: AssetImage('images/logo.png'),
+              ),
             ),
-            Center(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 30, 10, 20),
               child: Text(
                 phrases[_aleatoryNumber],
                 style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.center,
               ),
+            ),
+            const Text(
+              'Pressione o botão para gerar uma frase',
+              style: TextStyle(fontStyle: FontStyle.italic),
             ),
           ],
         ),
@@ -96,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
