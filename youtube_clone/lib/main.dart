ad22a044a19a5b67c289484d30f123c3971fa103
Lays Hidani <lays.hidani@gmail.com>
2021-11-07 17:30:16 -0300

    feat: add base home :tada:

---
+++ youtube_clone/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:youtube_clone/home.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
