f5e73b2cbe251e10f6e5dcd44963d183779c94c2
Lays Hidani <lays.hidani@gmail.com>
2021-12-28 11:39:47 -0300

    add base files and pages :sparkles:

---
+++ seu_lourival_1/lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static String REGISTER_PAGE_HERO_IMAGE = "assets/images/seu_lourival_1.jpg";
+  static String LOGIN_PAGE_HERO_IMAGE =
+      "assets/images/login_page_hero_image.jpg";
+
+  static String WEAK_PASSWORD = "Password is weak";
+  static String EMAIL_ALREADY_IN_USE = "Email is already in use";
+}
