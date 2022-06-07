d1fd2ec7846d49c547028b2c785f2de5eb748a43
Lays Hidani <lays.hidani@gmail.com>
2022-06-07 20:11:31 -0300

    init project

---
+++ expenses_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
